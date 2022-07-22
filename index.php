a694a7ddbeb2d55b2aa0ed45ca8022f623e55e1f
1Bipuldas <bipul439711@gmail.com>
2022-07-22 16:45:22 +0100

    all ok

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet"  href="new/icon/css/all.min.css">
+	<title>Blood bank</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<div class="login">
+			<div class="title">Login From</div>
+			<form class="login-from">
+				<input type="text" placeholder="Username Or Email Address">
+				<input type="Password" placeholder="Enter Password">
+				<a href="#">Forgt Password</a>
+				<input type="submit" value="LOGIN" class="login-btn">
+
+			</form>
+			<div class="social-btn">
+				<a href="#" target="_blank">Registration</a>
+			</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
